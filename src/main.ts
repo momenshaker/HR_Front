c5fdc57e12ee7a71aaade41c91305e2bd4ad30ca
Momen Shaker <momen_shaker@windowslive.com>
2025-10-30 13:45:08 +0300

    Fix HttpClient configuration to allow backend calls

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideHttpClient, withInterceptors, withRequestsMadeViaParent } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
@@ -13,10 +13,7 @@ import { provideMatSnackBar } from './app/shared/services/snackbar';
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(APP_ROUTES),
-    provideHttpClient(
-      withInterceptors([LoggingInterceptor, AuthInterceptor, ErrorInterceptor]),
-      withRequestsMadeViaParent()
-    ),
+    provideHttpClient(withInterceptors([LoggingInterceptor, AuthInterceptor, ErrorInterceptor])),
     provideAnimations(),
     provideAppConfig(),
     provideMatSnackBar()
