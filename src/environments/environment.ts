2b392575ab786ae992668ee331937f525c4222ea
momen shaker <mshaker@tygo.sa>
2025-10-30 12:32:27 +0300

    update endpoint path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'http://localhost:3000',
-  apiDefaultHeaders: {}
+  apiBaseUrl: 'https://localhost:60021/api/v1.0',
+  apiDefaultHeaders: {
+    'Idempotency-Key': 'ccd50775-e9fb-485e-a787-11e186801594'
+  }
 };
